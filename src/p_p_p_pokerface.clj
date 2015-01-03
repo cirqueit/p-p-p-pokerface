62d19c7d16e85d57acdb7343e381d56e2d025614
Joe Edwards <handbalancing@gmail.com>
2015-01-03 05:27:17 -0800

    passed 45/45

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,58 @@
 (ns p-p-p-pokerface)
 
+(def rank-values {"T" 10 "J" 11 "Q" 12 "K" 13 "A" 14})
+
 (defn rank [card]
-  nil)
+  (let [[f _] card
+        fst (str f)]
+    (if (Character/isDigit f)
+      (Integer/valueOf fst)
+      (rank-values fst))))
 
 (defn suit [card]
-  nil)
+  (let [[_ snd] card]
+    (str snd)))
 
 (defn pair? [hand]
-  nil)
+  (let [freqs (vals (frequencies (map rank hand)))]
+   (= 1 (count (filter #(= 2 %) freqs)))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (let [freqs (vals (frequencies (map rank hand)))]
+   (= 1 (count (filter #(= 3 %) freqs)))))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (let [freqs (vals (frequencies (map rank hand)))]
+   (= 1 (count (filter #(= 4 %) freqs)))))
 
 (defn flush? [hand]
-  nil)
+  (let [freqs (vals (frequencies (map suit hand)))]
+   (= 1 (count (filter #(= 5 %) freqs)))))
 
 (defn full-house? [hand]
-  nil)
+  (and (pair? hand) (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [freqs (vals (frequencies (map rank hand)))]
+   (= 2 (count (filter #(= 2 %) freqs)))))
 
 (defn straight? [hand]
-  nil)
+  (let [sorted-hand (sort (map rank hand))
+        low (first sorted-hand)
+        straight (range low (+ 5 low))]
+    (or (= sorted-hand straight) (= sorted-hand [2 3 4 5 14]))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (flush? hand) (straight? hand)))
+
+(defn high-card? [hand]
+  (let [freqs (vals (frequencies (map rank hand)))]
+    (and (not (flush? hand)) (= 1 (apply max freqs)))))
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]  [pair? 1]
+                   [two-pairs? 2]  [three-of-a-kind? 3]
+                   [straight? 4]   [flush? 5]
+                   [full-house? 6] [four-of-a-kind? 7]
+                   [straight-flush? 8]}]
+    (apply max (map second (filter #((first %) hand) checkers)))))
